95ea100a8ba8cc9a813ac640905fb5d2178a0577
olugashi <orenlugashi@gmail.com>
2020-06-30 17:01:37 +0300

    Add data converter and fix RTIConnector

--- RTIConnector.js
+++ RTIConnector.js
@@ -1,65 +1,60 @@
-import sleep from 'sleep'
-import { Connector } from 'rticonnextdds-connector'
-import { join } from 'path'
-const fullpath = join(__dirname, './ShapeExample.xml')
+import sleep from "sleep";
+import { Connector } from "rticonnextdds-connector";
 
-class RTIConnector
-{
-    constructor(socketsio)
-    {
-        this.socketsio = socketsio
-        this.connector = new Connector('MyParticipantLibrary::TransformationParticipant', fullpath)
+class RTIConnector {
+  constructor(socketsio, fullpath, participant) {
+    this.socketsio = socketsio;
+    this.outputs = [];
+    this.inputs = [];
+    this.connector = new Connector(participant, fullpath);
 
-     this.inputs = [
-        { input: this.connector.getInput('MySubscriber::MySquareReader'), topic: 'square' },
-        { input: this.connector.getInput('MySubscriber::MyTriangleReader'), topic: 'triangle' },
-        { input: this.connector.getInput('MySubscriber::MyCircleReader'), topic: 'circle' }
-      ]
-
-      this.outputs =  [
-        { output: this.connector.getOutput('MyPublisher::MyCircleWriter'), topic: 'circle' },
-        { output: this.connector.getOutput('MyPublisher::MySquareWriter'), topic: 'square' },
-        { output: this.connector.getOutput('MyPublisher::MyTriangleWriter'), topic: 'triangle' },
-      ]
-
-      this.connector.on('on_data_available', () => {
-        // We have received data on one of the inputs within this connector
-        // Iterate through each one, checking if it has any valid data
-        this.inputs.forEach(element => {
-          element.input.take()
-          for (const sample of element.input.samples.validDataIter) {
-            //console.log(sample.getJson())
-            this.socketsio.emit(element.topic, sample.getJson())
-          }
-        })})
-    }
-
-    writeData(data, type)
-    {
-      const run =async () => {
-       
-        try {
-         
-          const connector =this.outputs.find((element) => element.topic === type)
-          //console.log(connector.output)
-          //const output = this.connector.getOutput('MyPublisher::MySquareWriter')
-         
-          connector.output.instance.setNumber('x', data.x)
-          connector.output.instance.setNumber('y', data.y)
-          connector.output.instance.setNumber('shapesize', data.shapesize)
-          connector.output.instance.setString('color', data.color)
-          connector.output.write()
-          
-        ///console.log(connector.output.instance.getJson())
-        
-          // Wait for all subscriptions to receive the data before exiting
-          await connector.output.wait()
-        } catch (err) {
-          console.log('Error encountered: ' + err)
+    this.connector.on("on_data_available", () => {
+      // We have received data on one of the inputs within this connector
+      // Iterate through each one, checking if it has any valid data
+      this.inputs.forEach((element) => {
+        element.input.take();
+        for (const sample of element.input.samples.validDataIter) {
+          console.log(sample.getJson());
+          this.socketsio.emit(element.topic, sample.getJson());
         }
-        //connector.close()
-    }
-    run()
+      });
+    });
+  }
+
+  registerPublisher(output, topic, dataConverter) {
+    this.outputs.push({
+      output: this.connector.getOutput(output),
+      topic: topic,
+      dataConverter: dataConverter,
+    });
+  }
+  registerSubscriber(input, topic) {
+    this.inputs.push({
+      input: this.connector.getInput(input),
+      topic: topic,
+    });
+  }
+
+  writeData(data, type) {
+    const run = async () => {
+      try {
+        const connector = this.outputs.find(
+          (element) => element.topic === type
+        );
+        connector.dataConverter(connector, data);
+
+        // Wait for all subscriptions to receive the data before exiting
+        await connector.output.wait();
+      } catch (err) {
+        console.log("Error encountered: " + err);
+      }
+      //connector.close()
+    };
+    run();
+  }
+
+  close() {
+    this.connector.close();
   }
 }
 export default RTIConnector;
---
+++ ddsDataConverter/circleConverter.js
@@ -0,0 +1,9 @@
+function circleConverter(connector, data) {
+  connector.output.instance.setNumber("x", data.x);
+  connector.output.instance.setNumber("y", data.y);
+  connector.output.instance.setNumber("shapesize", data.shapesize);
+  connector.output.instance.setString("color", data.color);
+  connector.output.write();
+}
+
+module.exports = { circleConverter };
